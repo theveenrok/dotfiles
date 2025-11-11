844e38d4c2f23e19253ee6dd520dee488428ca36
Vlad Korneev <veenrok@veenrok.com>
2025-11-11 21:44:17 +0300

    feat(dotfiles/zsh): Remove unused configurations.

--- dot_config/zsh/conf.d/0003-zoxide.zsh
+++
@@ -1,6 +0,0 @@
-if ! command -v zoxide >/dev/null 2>&1; then
-    print "zoxide not installed"
-else
-    eval "$(zoxide init zsh)"
-fi
-
--- dot_config/zsh/conf.d/0004-fzf.zsh
+++
@@ -1,6 +0,0 @@
-if ! command -v fzf >/dev/null 2>&1; then
-    print "fzf not installed"
-else
-    source <(fzf --zsh)
-fi
-
